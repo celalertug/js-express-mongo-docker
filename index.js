a7141655208d63ecb1d720a1487a9ddc5a16a6d1
celalertug <celalertug@gmail.com>
2020-07-31 17:03:12 +0300

    added pm2

--- index.js
+++ index.js
@@ -13,26 +13,29 @@ const controller = require('./src/controller.js');
   } = process.env;
 
   let db;
+  let dbo;
   try {
     // db = await connectDb(process.env.MONGO_URL);
     db = await connectDb(MONGO_URL);
+    dbo = db.db(DB_NAME);
   } catch (error) {
     console.log('mongo db connection failed');
     process.exit(1);
   }
 
-  const dbo = db.db(DB_NAME);
-  const mongoService = create(dbo, COLLECTION_NAME);
-  await mongoService.createCollection();
-  // await createCollection(dbo, COLLECTION_NAME);
-
-  //   const dbo = db.db(process.env.DB_NAME);
+  const authTokenService = create(dbo, COLLECTION_NAME);
+  try {
+    await authTokenService.createCollection();
+  } catch (error) {
+    console.log('collection error');
+    console.log(error);
+  }
 
   const app = express();
 
   app.use(bodyParser.json());
   app.use(cors());
-  app.use('/', controller(mongoService));
+  app.use('/', controller(authTokenService));
 
   app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
 })();
